c16ab6e90ad354f5e413704f0629346da491b3f5
mainavict <vampmid03@gmail.com>
2023-03-16 13:34:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -A program that prints aline with puts function
- * Return: 0 (Succes)
+ * main  - A program that prints aline with puts function
+ * Return: 0 (Success)
  */
 int  main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
